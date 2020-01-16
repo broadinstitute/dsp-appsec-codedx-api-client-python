67cf4cd5b1f07148ba315be1c321a9aa24a24551
Sarada Symonds <ssymonds@wmb37-38e.broadinstitute.org>
2020-01-16 14:00:42 -0500

    trying to fix file permissions

--- apiclient/APIs/CodeDXClient.py
+++ apiclient/APIs/CodeDXClient.py
@@ -30,24 +30,28 @@ class CodeDX(ProjectsAPI.Projects, ReportsAPI.Reports, JobsAPI.Jobs, AnalysisAPI
 			print("using wb permissions")
 			with open(file_name, 'wb') as f:
 					f.write(data)
-		except:
+		except Exception, e:
+			print(str(e))
 			try:
 				print("using w+ permissions")
 				with open(file_name, 'w+') as f:
 						f.write(data)
-			except:
+			except Exception, e:
+				print(str(e))
 				try:
 					print("adding dir to filename")
 					fn = '/reports/' + file_name
 					with open(fn, 'wb') as f:
 							f.write(data)
-				except:
+				except Exception, e:
+					print(str(e))
 					try:
 						print("adding dir to filename w+")
 						fn = '/reports/' + file_name
 						with open(fn, 'w+') as f:
 								f.write(data)
-					except:
+					except Exception, e:
+						print(str(e))
 						print("none worked")
 
 	def get_report(self, job, content_type, file_name, msg):
